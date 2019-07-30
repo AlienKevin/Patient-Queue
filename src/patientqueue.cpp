ad1603eaedb20fa7bcc551f95272b51785c08c82
Kevin Li <kevinli020508@gmail.com>
2019-07-29 21:50:11 -0700

    Initial commit

---
+++ src/patientqueue.cpp
@@ -0,0 +1,63 @@
+// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+
+#include "patientqueue.h"
+
+PatientQueue::PatientQueue(bool isMinHeap) {
+    // TODO: write this constructor
+
+}
+
+PatientQueue::~PatientQueue() {
+    // TODO: write this destructor
+
+}
+
+void PatientQueue::changePriority(string value, int newPriority) {
+    // TODO: write this function
+
+}
+
+void PatientQueue::clear() {
+    // TODO: write this function
+
+}
+
+void PatientQueue::debug() {
+    // empty
+}
+
+string PatientQueue::dequeue() {
+    // TODO: write this function
+    return "";   // remove this
+}
+
+void PatientQueue::enqueue(string value, int priority) {
+    // TODO: write this function
+
+}
+
+bool PatientQueue::isEmpty() const {
+    // TODO: write this function
+    return false;   // remove this
+}
+
+string PatientQueue::peek() const {
+    // TODO: write this function
+    return "";   // remove this
+}
+
+int PatientQueue::peekPriority() const {
+    // TODO: write this function
+    return 0;   // remove this
+}
+
+int PatientQueue::size() const {
+    // TODO: write this function
+    return 0;   // remove this
+}
+
+ostream& operator <<(ostream& out, const PatientQueue& queue) {
+    // TODO: write this function
+
+    return out;
+}
