171b42f3847a19dcf36ad543aa4e588b0eaccb14
Kevin Li <kevinli020508@gmail.com>
2019-07-30 21:18:43 -0700

    Add doc to program

--- src/patientqueue.cpp
+++ src/patientqueue.cpp
@@ -1,15 +1,44 @@
-// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+/**
+ * Patient Queue assignment for CS106B at Stanford Summer Session 2019
+ * Author: Kevin Li
+ * Description:
+ * This program stores a patient queue that manages a waiting line of patients
+ * to be processed at a hospital. As each new patient checks in at the hospital,
+ * the staff assesses their injuries and gives that patient an integer priority
+ * rating, with smaller integers representing greater urgency (a min heap).
+ * You can switch the order and make larger integers representing
+ * greater urgency (a max heap) by passing true to PatientQueue constructor.
+ * Note that a min heap always has the patient with the smallest priority value in front
+ * and a max heap always has patient with the largest priority value in front.
+ * Usually, smaller priority value indicates more urgency but max heap is justified
+ * because less urgent patients may be treated more quickly.
+ */
 #include "patientqueue.h"
 
+/**
+ * @brief Constructor for the PatientQueue
+ * @param isMinHeap indicate whether to use min heap or max heap
+ */
 PatientQueue::PatientQueue(bool isMinHeap) {
     this->isMinHeap = isMinHeap;
     clear();
 }
 
+/**
+ * @brief Destructor for the PatientQueue
+ */
 PatientQueue::~PatientQueue() {
     delete[] patients;
 }
 
+/**
+ * @brief Change a patient's priority by:
+ *  1. finding the patient by their name
+ *  2. bubble the patient up or down depending on the new priority
+ * @param value the patient's name
+ * @param newPriority the patient's new priority
+ * @throw string error if patient name if not found in the queue
+ */
 void PatientQueue::changePriority(string value, int newPriority) {
     for (int i = 1; i < patientsSize + 1; i ++) {
         Patient patient = patients[i];
@@ -26,15 +55,26 @@ void PatientQueue::changePriority(string value, int newPriority) {
     throw "Patient " + value + " is not found in the patients queue!";
 }
 
+/**
+ * @brief Clear the patients queue by reseting private member variables
+ */
 void PatientQueue::clear() {
     patientsCapacity = 10;
     patientsSize = 0;
     patients = new Patient[patientsCapacity];
 }
 
+/**
+ * @brief Function for printing debugging messages
+ */
 void PatientQueue::debug() {
 }
 
+/**
+ * @brief Bubble the patient at currentPos down by
+ * comparing the patient's priority with their parent's
+ * @param currentPos the current position of the patient to bubble down
+ */
 void PatientQueue::bubbleDown(int currentPos) {
     int parentPos = currentPos / 2;
     while (parentPos >= 1 &&
@@ -46,6 +86,11 @@ void PatientQueue::bubbleDown(int currentPos) {
     }
 }
 
+/**
+ * @brief Bubble the patient at currentPos up by
+ * comparing the patient's priority with their children's
+ * @param currentPos the current position of the patient to bubble up
+ */
 void PatientQueue::bubbleUp(int currentPos) {
     while ((currentPos * 2 <= patientsSize &&
             hasLowerOrEqualPriority(currentPos, currentPos * 2)) ||
@@ -82,6 +127,13 @@ void PatientQueue::bubbleUp(int currentPos) {
     }
 }
 
+/**
+ * @brief Dequeue the front patient who has the most urgent priority by
+ *  1. popping the front patient and move the last patient to the front
+ *  2. Bubble the last patient down
+ * @return the name of the front patient
+ * @throw string error if queue is empty
+ */
 string PatientQueue::dequeue() {
     if (isEmpty()) {
         throw "Cannot dequeue an empty queue!";
@@ -96,6 +148,12 @@ string PatientQueue::dequeue() {
     return frontPatient.name;
 }
 
+/**
+ * @brief Test if the patient at pos1 has a higher priority than the patient at pos2
+ * @param pos1 the index position of the first patient
+ * @param pos2 the index position of the second patient
+ * @return if the patient at pos1 has a higher priority than the patient at pos2
+ */
 bool PatientQueue::hasHigherPriority(int pos1, int pos2) {
     if (isMinHeap) {
         return patients[pos1].priority < patients[pos2].priority ||
@@ -108,10 +166,22 @@ bool PatientQueue::hasHigherPriority(int pos1, int pos2) {
     }
 }
 
+/**
+ * @brief Test if the patient at pos1 has a higher or equal priority than the patient at pos2
+ * @param pos1 the index position of the first patient
+ * @param pos2 the index position of the second patient
+ * @return if the patient at pos1 has a higher priority or equal than the patient at pos2
+ */
 bool PatientQueue::hasHigherOrEqualPriority(int pos1, int pos2) {
     return !hasLowerPriority(pos1, pos2);
 }
 
+/**
+ * @brief Test if the patient at pos1 has a lower priority than the patient at pos2
+ * @param pos1 the index position of the first patient
+ * @param pos2 the index position of the second patient
+ * @return if the patient at pos1 has a lower priority than the patient at pos2
+ */
 bool PatientQueue::hasLowerPriority(int pos1, int pos2) {
     if (isMinHeap) {
         return patients[pos1].priority > patients[pos2].priority ||
@@ -124,10 +194,21 @@ bool PatientQueue::hasLowerPriority(int pos1, int pos2) {
     }
 }
 
+/**
+ * @brief Test if the patient at pos1 has a lower or equal priority than the patient at pos2
+ * @param pos1 the index position of the first patient
+ * @param pos2 the index position of the second patient
+ * @return if the patient at pos1 has a lower or equal priority than the patient at pos2
+ */
 bool PatientQueue::hasLowerOrEqualPriority(int pos1, int pos2) {
     return !hasHigherPriority(pos1, pos2);
 }
 
+/**
+ * @brief Swap the patient at pos1 with the patient at pos2
+ * @param pos1 the index position of the first patient
+ * @param pos2 the index position of the second patient
+ */
 void PatientQueue::swap(int &pos1, int pos2) {
     Patient pos1Patient = patients[pos1];
     // swap pos1 position with pos2 position
@@ -137,7 +218,12 @@ void PatientQueue::swap(int &pos1, int pos2) {
     pos1 = pos2;
 }
 
-// swap current position (pos1) with parent (pos2)
+/**
+ * @brief Swap current position with parent if current has a higher priority
+ * @param currentPos the index position of the current patient
+ * @param parentPos the index position of current patient's parent
+ * @return the swap is successful or not
+ */
 bool PatientQueue::swapDown(int &currentPos, int parentPos) {
     if (hasHigherPriority(currentPos, parentPos)) {
         swap(currentPos, parentPos);
@@ -146,6 +232,12 @@ bool PatientQueue::swapDown(int &currentPos, int parentPos) {
     return false;
 }
 
+/**
+ * @brief Swap current position with a child if current's child has a higher priority
+ * @param currentPos the index position of the current patient
+ * @param childPos the index position of current patient's child
+ * @return the swap is successful or not
+ */
 bool PatientQueue::swapUp(int &currentPos, int childPos) {
     int childPosCopy = childPos;
     if (swapDown(childPosCopy, currentPos)) {
@@ -155,6 +247,11 @@ bool PatientQueue::swapUp(int &currentPos, int childPos) {
     return false;
 }
 
+/**
+ * @brief Resize the patients queue represented by an array to fit more patients by
+ *  1. Copy all patients in the current array to a new array
+ *  2. Delete the old array
+ */
 void PatientQueue::resize() {
     // double the capacity
     patientsCapacity *= 2;
@@ -166,6 +263,13 @@ void PatientQueue::resize() {
     patients = newPatients;
 }
 
+/**
+ * @brief Enqueue a new patient to the patient queue by
+ *  1. Put the patient at the end of the queue
+ *  2. Bubble the patient up
+ * @param value the name of the new patient
+ * @param priority the priority of the new patient
+ */
 void PatientQueue::enqueue(string value, int priority) {
     if (patientsSize + 1 == patientsCapacity) { // patients queue full
         resize();
@@ -177,10 +281,21 @@ void PatientQueue::enqueue(string value, int priority) {
     patientsSize ++;
 }
 
+/**
+ * @brief Test if the patient queue is empty or not by
+ * testing if the size of the array representing the queue is equal to 0
+ * @return if the patient queue is empty or not
+ */
 bool PatientQueue::isEmpty() const {
     return patientsSize == 0;
 }
 
+/**
+ * @brief Peek at the front patient of the queue and return their name
+ * Note the the patient queue starts at index 1 not index 0 for calculation convenience
+ * @return the name of the front patient
+ * @throw string error if queue is empty
+ */
 string PatientQueue::peek() const {
     if (isEmpty()) {
         throw "Cannot peek at an empty queue!";
@@ -188,6 +303,11 @@ string PatientQueue::peek() const {
     return patients[1].name;
 }
 
+/**
+ * @brief Peek at the front patient of the queue and return their priority value
+ * Note the the patient queue starts at index 1 not index 0 for calculation convenience
+ * @return the priority value of the front patient
+ */
 int PatientQueue::peekPriority() const {
     if (isEmpty()) {
         throw "Cannot peek at an empty queue!";
@@ -195,11 +315,18 @@ int PatientQueue::peekPriority() const {
     return patients[1].priority;
 }
 
+/**
+ * @brief return the size of the patient queue
+ * @return the size of the patient queue
+ */
 int PatientQueue::size() const {
     return patientsSize;
 }
 
-// {Anat (4), Rein (6), Sasha (8),	Ben (9), Wu (7), Eve (10)}
+/**
+ * Print the patient queue in a nice format:
+ * e.g. {Anat (4), Rein (6), Sasha (8),	Ben (9), Wu (7), Eve (10)}
+ */
 ostream& operator <<(ostream& out, const PatientQueue& queue) {
     out << "{";
     for (int i = 1; i < queue.patientsSize + 1; i ++) {
--- src/patientqueue.h
+++ src/patientqueue.h
@@ -1,4 +1,18 @@
-// This is the H file you will edit and turn in. (TODO: Remove this comment!)
+/**
+ * Patient Queue assignment for CS106B at Stanford Summer Session 2019
+ * Author: Kevin Li
+ * Description:
+ * This program stores a patient queue that manages a waiting line of patients
+ * to be processed at a hospital. As each new patient checks in at the hospital,
+ * the staff assesses their injuries and gives that patient an integer priority
+ * rating, with smaller integers representing greater urgency (a min heap).
+ * You can switch the order and make larger integers representing
+ * greater urgency (a max heap) by passing true to PatientQueue constructor.
+ * Note that a min heap always has the patient with the smallest priority value in front
+ * and a max heap always has patient with the largest priority value in front.
+ * Usually, smaller priority value indicates more urgency but max heap is justified
+ * because less urgent patients may be treated more quickly.
+ */
 
 #ifndef _patientqueue_h
 #define _patientqueue_h
@@ -10,23 +24,87 @@ using namespace std;
 
 class PatientQueue {
 public:
+    /**
+     * @brief PatientQueue constructor
+     * @param isMinHeap indicates whether to use a min heap or max heap
+     */
     PatientQueue(bool isMinHeap = true);
+    /**
+     * @brief PatientQueue destructor
+     * Deallocates used heap memory
+     */
     ~PatientQueue();
+    /**
+     * @brief Change the priority of a patient in the queue
+     * changePriority affect only a single occurrence of
+     * a patient's name (the first one found)
+     * @param value the name of the patient
+     * @param newPriority the new priority value
+     */
     void changePriority(string value, int newPriority);
+    /**
+     * @brief Clear the patient queue
+     */
     void clear();
+    /**
+     * @brief Method used for debuggin
+     */
     void debug();
+    /**
+     * @brief Give back the front patient with the most urgent priority
+     * @return the name of the front patient
+     */
     string dequeue();
+    /**
+     * @brief Add a new patient to the queue
+     * @param value the name of the patient
+     * @param priority the priority value of the patient
+     */
     void enqueue(string value, int priority);
+    /**
+     * @brief Return is the patient queue is empty or not
+     * @return the patient queue is empty or not
+     */
     bool isEmpty() const;
+    /**
+     * @brief Return the front patient's name
+     * @return the front patient's name
+     */
     string peek() const;
+    /**
+     * @brief Return the front patient's priority
+     * @return the front patient's priority
+     */
     int peekPriority() const;
+    /**
+     * @brief Return the size of the patient queue
+     * @return the size of the patient queue
+     */
     int size() const;
+    /**
+     * @brief Print the patient queue in a nice format
+     * @param out the out stream for printing
+     * @param queue the patient queue to print
+     * @return the out stream for printing
+     */
     friend ostream& operator <<(ostream& out, const PatientQueue& queue);
 
 private:
+    /**
+     * @brief the patient queue implemented as an array
+     */
     Patient *patients;
+    /**
+     * @brief the size of the patient queue
+     */
     int patientsSize;
+    /**
+     * @brief the capacity of the patient queue array
+     */
     int patientsCapacity;
+    /**
+     * @brief Whether to use a min heap or max heap
+     */
     bool isMinHeap;
     bool hasHigherPriority(int pos1, int pos2);
     bool hasHigherOrEqualPriority(int pos1, int pos2);
